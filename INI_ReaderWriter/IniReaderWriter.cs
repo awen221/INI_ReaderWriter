277d5fee0b1b1028e101f7b15bfaf670359b80af
larry.lai <larry.lai@advantech.com>
2020-11-25 15:39:50 +0800

    建立INI讀寫基本功能

---
+++ INI_ReaderWriter/IniReaderWriter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Ini;
+
+namespace INI_ReaderWriter
+{
+    abstract public class IniReaderWriter
+    {
+        string colon => ":";
+        IniConfigurationProvider iniProvider { set; get; }
+        abstract protected string iniPath { get; }
+
+        public IniReaderWriter()
+        {
+            IniConfigurationSource source = new IniConfigurationSource
+            {
+                Path = iniPath,
+            };
+            iniProvider = (IniConfigurationProvider)source.Build(new ConfigurationBuilder());
+            iniProvider.Load();
+        }
+
+        public bool TryGet(string section, string key, out string value)
+        {
+            key = section + colon + key;
+            return iniProvider.TryGet(key, out value);
+        }
+
+        public void Set(string section, string key, string value)
+        {
+            key = section + colon + key;
+            iniProvider.Set(key, value);
+        }
+    }
+}
